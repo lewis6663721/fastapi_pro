dfc0c338f48cc0a20e78a2ff8b54c8b082bffbea
lyh <1363998021@qq.com>
2023-09-03 14:44:51 +0800

    add main files

---
+++ fastapi_pro/main.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles())
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", reload=True, workers=1)
