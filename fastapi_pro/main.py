b4a5b33c2ef97ab3ae6a68ec44be96f946441bb3
lyh <1363998021@qq.com>
2024-03-27 22:38:37 +0800

    add test router

--- fastapi_pro/main.py
+++ fastapi_pro/main.py
@@ -1,11 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from routers.test.views import router as test_router
 
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles())
+app.include_router(prefix="/test_router", router=test_router)
 
 
 if __name__ == '__main__':
---
+++ fastapi_pro/routers/test/views.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+
+router = APIRouter()
+
+
+@router.get("/test")
+async def test():
+    return {"hello": "world"}
